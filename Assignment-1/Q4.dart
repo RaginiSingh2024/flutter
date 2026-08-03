c37aacde5fdce2a1540bde0c4fc94b5933f3ce5d
Ragini Singh <raginisingh.sejal@gmail.com>
2026-08-03 22:21:48 +0530

    Add multiplication tables for numbers 1 to 10

--- Assignment-1/Q4.dart
+++ Assignment-1/Q4.dart
@@ -12,3 +12,38 @@ void main() {
     print("");
   }
 }
+
+// Q4 Output
+// Multiplication Table of 1
+// 1 x 1 = 1
+// 1 x 2 = 2
+// 1 x 3 = 3
+// 1 x 4 = 4
+// 1 x 5 = 5
+// 1 x 6 = 6
+// 1 x 7 = 7
+// 1 x 8 = 8
+// 1 x 9 = 9
+// 1 x 10 = 10
+
+// Multiplication Table of 2
+// 2 x 1 = 2
+// 2 x 2 = 4
+// ...
+// 2 x 10 = 20
+
+// Multiplication Table of 3
+// ...
+// ...
+
+// Multiplication Table of 10
+// 10 x 1 = 10
+// 10 x 2 = 20
+// 10 x 3 = 30
+// 10 x 4 = 40
+// 10 x 5 = 50
+// 10 x 6 = 60
+// 10 x 7 = 70
+// 10 x 8 = 80
+// 10 x 9 = 90
+// 10 x 10 = 100
